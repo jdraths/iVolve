61a1a3fdbc3dca82ffe9b3f948775614e9a593c4
jdraths <jdraths@alumni.nd.edu>
2013-10-01 21:34:15 -0700

    comment out signin with Twitter Github

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,13 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def client    #added from Sign-in with Twitter github sferik
-  	Twitter.configure do |config|
-  		config.consumer_key = ENV['CONSUMER_KEY']
-  		config.consumer_secret = ENV['CONSUMER_SECRET']
-  		config.oauth_token = session['access_token']
-  		config.oauth_token_secret = session['access_secret']
-  	end
-  end
+#  def client    #added from Sign-in with Twitter github sferik
+#  	Twitter.configure do |config|
+#  		config.consumer_key = ENV['CONSUMER_KEY']
+#  		config.consumer_secret = ENV['CONSUMER_SECRET']
+#  		config.oauth_token = session['access_token']
+#  		config.oauth_token_secret = session['access_secret']
+#  	end
+#  end
   
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,23 +13,23 @@ class SessionsController < ApplicationController
 			render 'new'
 		end
 #------------ below added with Sign-In with Twitter Github
-		session[:access_token] = request.env['omniauth.auth']['credentials']['token']
-    	session[:access_secret] = request.env['omniauth.auth']['credentials']['secret']
-   		redirect_to show_path, notice: "Signed in"
-	end
-
-	def show
-		if session['access_token'] && session['access_secret']
-      		@user = client.user(include_entities: true)
-    	else
-      		redirect_to failure_path
-    	end
-  	end
-
-	def error
-	    flash[:error] = "Sign in with Twitter failed"
-	    redirect_to root_path
-	end
+#		session[:access_token] = request.env['omniauth.auth']['credentials']['token']
+#    	session[:access_secret] = request.env['omniauth.auth']['credentials']['secret']
+#   		redirect_to show_path, notice: "Signed in"
+#	end
+#
+#	def show
+#		if session['access_token'] && session['access_secret']
+#      		@user = client.user(include_entities: true)
+#    	else
+#      		redirect_to failure_path
+#    	end
+#  	end
+#
+#	def error
+#	    flash[:error] = "Sign in with Twitter failed"
+#	    redirect_to root_path
+#	end
 #------------- above
 	def destroy
 		sign_out
