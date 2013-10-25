ad1b1c73eaa6bd8245e3c7d31c75f3656ccf5464
jdraths <jdraths@alumni.nd.edu>
2013-10-25 01:12:45 -0700

    change twitter

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,15 +14,6 @@ class StaticPagesController < ApplicationController
         @twitter_sent = @twitter_user.tweet_count
         @twitter_favorites = @twitter_user.favorite_count
         @twitter_lists = @twitter_user.listed_count
-      else
-        @twitter_user = nil
-        @twitter_name = nil
-        @twitter_description = nil
-        @twitter_friends = nil
-        @twitter_followers = nil
-        @twitter_sent = nil
-        @twitter_favorites = nil
-        @twitter_lists = nil
       end
   	end
   end
