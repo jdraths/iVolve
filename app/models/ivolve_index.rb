398b6bd59f251c6064ed5ce66ff2d7d8d7d5b692
jdraths <jdraths@alumni.nd.edu>
2013-11-08 22:26:36 -0800

    add ivolve index daily tracking

---
+++ app/models/ivolve_index.rb
@@ -0,0 +1,123 @@
+class IvolveIndex < ActiveRecord::Base
+	default_scope -> { order('created_at DESC') }
+
+	def self.avg_stats(db, column)
+		case db
+			when 'twitter_db'
+				database = @twitter_users 
+				db_size = @twitter_num_users
+			when 'facebook_db'
+				database = @facebook_users
+				db_size = @facebook_num_users
+			when 'instagram_db'
+				database = @instagram_users
+				db_size = @instagram_num_users
+			when 'fitbit_db'
+				database = @fitbit_users
+				db_size = @fitbit_num_users
+		end
+		database.sum(column)/db_size
+	end
+	
+	def self.populate_data
+
+		@twitter_users = TwitterUser.where('created_at >= ?', TwitterUser.last.created_at.beginning_of_day)
+		@facebook_users = FacebookUser.where('created_at >= ?', FacebookUser.last.created_at.beginning_of_day)
+		@instagram_users = InstagramUser.where('created_at >= ?', InstagramUser.last.created_at.beginning_of_day)
+		@fitbit_users = FitbitUser.where('created_at >= ?', FitbitUser.last.created_at.beginning_of_day)
+
+# MOVE THESE SIZE METHODS INTO avg_stats if they aren't directly used in self.populate_data
+		@twitter_num_users = @twitter_users.size
+		@facebook_num_users = @facebook_users.size
+		@instagram_num_users = @instagram_users.size
+		@fitbit_num_users = @fitbit_users.size
+		# TWITTER STATS BELOW	
+		@iv_twitter_friends = avg_stats('twitter_db', 'friends_int_count')	
+		@iv_twitter_follwers = avg_stats('twitter_db', 'followers_int_count')
+		@iv_twitter_tweets_sent = avg_stats('twitter_db', 'tweet_int_count')
+		@iv_twitter_tweets_favd = avg_stats('twitter_db', 'favorite_int_count')
+		@iv_twitter_lists = avg_stats('twitter_db', 'listed_int_count')
+		# FACEBOOK STATS BELOW		
+		@iv_facebook_friends = avg_stats('facebook_db', 'int_friends')
+		@iv_facebook_likes_sent = avg_stats('facebook_db', 'int_likes')
+		@iv_facebook_posts = avg_stats('facebook_db', 'int_posts')
+		@iv_facebook_statuses = avg_stats('facebook_db', 'int_statuses')
+		@iv_facebook_achievements = avg_stats('facebook_db', 'int_achievements')
+		@iv_facebook_subscribers  = avg_stats('facebook_db', 'int_subscribers')
+		@iv_facebook_subscribed_to_you  = avg_stats('facebook_db', 'int_subscribed_to')
+		# INSTAGRAM STATS BELOW
+		@iv_instagram_followers = avg_stats('instagram_db', 'int_followers')
+		@iv_instagram_following = avg_stats('instagram_db', 'int_following')
+		@iv_instagram_shared = avg_stats('instagram_db', 'int_media')
+		@iv_instagram_likes_sent = avg_stats('instagram_db', 'int_likes_out')
+		# FITBIT STATS BELOW
+		@iv_fitbit_height = avg_stats('fitbit_db', 'height_int')
+		@iv_fitbit_weight = avg_stats('fitbit_db', 'weight_int')
+		@iv_fitbit_stride_length_run = avg_stats('fitbit_db', 'stride_length_run_int')
+		@iv_fitbit_stride_length_walk = avg_stats('fitbit_db', 'stride_length_walk_int')
+		@iv_fitbit_life_tot_active_score = avg_stats('fitbit_db', 'lifetime_tot_active_score_int')
+		@iv_fitbit_best_tot_active_score = avg_stats('fitbit_db', 'best_tot_active_score_int')
+		@iv_fitbit_life_tot_cal_out = avg_stats('fitbit_db', 'lifetime_tot_cal_out_int')
+		@iv_fitbit_best_tot_cal_out = avg_stats('fitbit_db', 'best_tot_cal_out_int')
+		@iv_fitbit_life_tot_dist = avg_stats('fitbit_db', 'lifetime_tot_dist_int')
+		@iv_fitbit_best_tot_dist = avg_stats('fitbit_db', 'best_tot_dist_int')
+		@iv_fitbit_life_tot_steps = avg_stats('fitbit_db', 'lifetime_tot_steps_int')
+		@iv_fitbit_best_tot_steps = avg_stats('fitbit_db', 'best_tot_steps_int')
+
+		create!(
+			iv_total: nil,
+			iv_physical: nil,
+			iv_intellectual: nil,
+			iv_social: nil,
+			iv_spiritual: nil,
+			iv_creative: nil,
+			iv_emotional: nil,
+			male_total: nil,
+			male_physical: nil,
+			male_intellectual: nil,
+			male_social: nil,
+			male_spiritual: nil,
+			male_creative: nil,
+			male_emotional: nil,
+			female_total: nil,
+			female_physical: nil,
+			female_intellectual: nil,
+			female_social: nil,
+			female_spiritual: nil,
+			female_creative: nil,
+			female_emotional: nil,
+			twitter_users_total: @twitter_num_users,
+			facebook_users_total: @facebook_num_users,
+			instagram_users_total: @instagram_num_users,
+			fitbit_users_total: @fitbit_num_users,
+			iv_twitter_friends: @iv_twitter_friends,
+			iv_twitter_follwers: @iv_twitter_follwers,
+			iv_twitter_tweets_sent: @iv_twitter_tweets_sent,
+			iv_twitter_tweets_favd: @iv_twitter_tweets_favd,
+			iv_twitter_lists: @iv_twitter_lists,
+			iv_facebook_friends: @iv_facebook_friends,
+			iv_facebook_likes_sent: @iv_facebook_likes_sent,
+			iv_facebook_posts: @iv_facebook_posts,
+			iv_facebook_statuses: @iv_facebook_statuses,
+			iv_facebook_achievements: @iv_facebook_achievements,
+			iv_facebook_subscribers: @iv_facebook_subscribers,
+			iv_facebook_subscribed_to_you: @iv_facebook_subscribed_to_you,
+			iv_instagram_followers: @iv_instagram_followers,
+			iv_instagram_following: @iv_instagram_following,
+			iv_instagram_shared: @iv_instagram_shared,
+			iv_instagram_likes_sent: @iv_instagram_likes_sent,
+			iv_fitbit_height: @iv_fitbit_height,
+			iv_fitbit_weight: @iv_fitbit_weight,
+			iv_fitbit_stride_length_run: @iv_fitbit_stride_length_run,
+			iv_fitbit_stride_length_walk: @iv_fitbit_stride_length_walk,
+			iv_fitbit_life_tot_active_score: @iv_fitbit_life_tot_active_score,
+			iv_fitbit_best_tot_active_score: @iv_fitbit_best_tot_active_score,
+			iv_fitbit_life_tot_cal_out: @iv_fitbit_life_tot_cal_out,
+			iv_fitbit_best_tot_cal_out: @iv_fitbit_best_tot_cal_out,
+			iv_fitbit_life_tot_dist: @iv_fitbit_life_tot_dist,
+			iv_fitbit_best_tot_dist: @iv_fitbit_best_tot_dist,
+			iv_fitbit_life_tot_steps: @iv_fitbit_life_tot_steps,
+			iv_fitbit_best_tot_steps: @iv_fitbit_best_tot_steps,
+		)
+	end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131107151203) do
+ActiveRecord::Schema.define(version: 20131108153238) do
 
   create_table "activities", force: true do |t|
     t.integer  "user_id"
@@ -265,6 +265,64 @@ ActiveRecord::Schema.define(version: 20131107151203) do
     t.integer  "int_following"
     t.integer  "int_media"
     t.integer  "int_likes_out"
+  end
+
+  create_table "ivolve_indices", force: true do |t|
+    t.integer  "iv_total"
+    t.integer  "iv_physical"
+    t.integer  "iv_intellectual"
+    t.integer  "iv_social"
+    t.integer  "iv_spiritual"
+    t.integer  "iv_creative"
+    t.integer  "iv_emotional"
+    t.integer  "male_total"
+    t.integer  "male_physical"
+    t.integer  "male_intellectual"
+    t.integer  "male_social"
+    t.integer  "male_spiritual"
+    t.integer  "male_creative"
+    t.integer  "male_emotional"
+    t.integer  "female_total"
+    t.integer  "female_physical"
+    t.integer  "female_intellectual"
+    t.integer  "female_social"
+    t.integer  "female_spiritual"
+    t.integer  "female_creative"
+    t.integer  "female_emotional"
+    t.integer  "twitter_users_total"
+    t.integer  "facebook_users_total"
+    t.integer  "instagram_users_total"
+    t.integer  "fitbit_users_total"
+    t.integer  "iv_twitter_friends"
+    t.integer  "iv_twitter_follwers"
+    t.integer  "iv_twitter_tweets_sent"
+    t.integer  "iv_twitter_tweets_favd"
+    t.integer  "iv_twitter_lists"
+    t.integer  "iv_facebook_friends"
+    t.integer  "iv_facebook_likes_sent"
+    t.integer  "iv_facebook_posts"
+    t.integer  "iv_facebook_statuses"
+    t.integer  "iv_facebook_achievements"
+    t.integer  "iv_facebook_subscribers"
+    t.integer  "iv_facebook_subscribed_to_you"
+    t.integer  "iv_instagram_followers"
+    t.integer  "iv_instagram_following"
+    t.integer  "iv_instagram_shared"
+    t.integer  "iv_instagram_likes_sent"
+    t.integer  "iv_fitbit_height"
+    t.integer  "iv_fitbit_weight"
+    t.integer  "iv_fitbit_stride_length_run"
+    t.integer  "iv_fitbit_stride_length_walk"
+    t.integer  "iv_fitbit_life_tot_active_score"
+    t.integer  "iv_fitbit_best_tot_active_score"
+    t.integer  "iv_fitbit_life_tot_cal_out"
+    t.integer  "iv_fitbit_best_tot_cal_out"
+    t.integer  "iv_fitbit_life_tot_dist"
+    t.integer  "iv_fitbit_best_tot_dist"
+    t.integer  "iv_fitbit_life_tot_steps"
+    t.integer  "iv_fitbit_best_tot_steps"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "microposts", force: true do |t|
