a6c551fd0c41af43a04099bd18e7c2b95a68f3be
jdraths <jdraths@alumni.nd.edu>
2013-11-12 20:28:19 -0800

    graph changes

--- app/models/twitter_user.rb
+++ app/models/twitter_user.rb
@@ -8,7 +8,7 @@ class TwitterUser < ActiveRecord::Base
 	def self.total_grouped_by_date(start)
 		data = where(created_at: start.beginning_of_day..Time.zone.now)
 		data = data.group("date(created_at)")
-		data = data.select("created_at, sum(favorite_int_count) as favorite_int_count, sum(followers_int_count) as followers_int_count, sum(friends_int_count) as friends_int_count, sum(listed_int_count) as listed_int_count, sum(tweet_int_count) as tweet_int_count")
+		data = data.select("date(created_at) as created_at, sum(favorite_int_count) as favorite_int_count, sum(followers_int_count) as followers_int_count, sum(friends_int_count) as friends_int_count, sum(listed_int_count) as listed_int_count, sum(tweet_int_count) as tweet_int_count")
 		data.group_by { |d| d.created_at.to_date }
 	end
 
