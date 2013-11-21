3b679336bc11ab81ac150d1b9f40024abecac1df
jdraths <jdraths@alumni.nd.edu>
2013-11-21 06:39:14 -0800

    social engagement graph

--- app/assets/javascripts/stats_display.js.coffee
+++ app/assets/javascripts/stats_display.js.coffee
@@ -39,5 +39,19 @@ jQuery ->
         {label: "Instagram", value: $('#ivolve-connections').data('instagram')}
         {label: "Foursquare", value: $('#ivolve-connections').data('foursquare')}
     ]
-
-
+  Morris.Donut
+    element: 'social-engagement'
+    data: [
+        {label: "Twitter", value: $('#social-engagement').data('twitter')}
+        {label: "Facebook", value: $('#social-engagement').data('facebook')}
+        {label: "Instagram", value: $('#social-engagement').data('instagram')}
+        {label: "Foursquare", value: $('#social-engagement').data('foursquare')}
+    ]
+  Morris.Donut
+    element: 'ivolve-engagement'
+    data: [
+        {label: "Twitter", value: $('#ivolve-engagement').data('twitter')}
+        {label: "Facebook", value: $('#ivolve-engagement').data('facebook')}
+        {label: "Instagram", value: $('#ivolve-engagement').data('instagram')}
+        {label: "Foursquare", value: $('#ivolve-engagement').data('foursquare')}
+    ]
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -18,12 +18,19 @@ class StaticPagesController < ApplicationController
         @twitter_avg_fav = @ivolveindex.iv_twitter_tweets_favd
         @twitter_int_lists = @twitter_auth_user.listed_int_count
         @twitter_avg_lists = @ivolveindex.iv_twitter_lists
+      else
+        @twitter_int_friends = 0
+        @twitter_avg_friends = @ivolveindex.iv_twitter_friends
+        @twitter_int_followers = 0
+        @twitter_avg_followers = @ivolveindex.iv_twitter_follwers
+        @twitter_int_sent = 0
+        @twitter_avg_sent = @ivolveindex.iv_twitter_tweets_sent
+        @twitter_int_favorites = 0
+        @twitter_avg_fav = @ivolveindex.iv_twitter_tweets_favd
+        @twitter_int_lists = 0
+        @twitter_avg_lists = @ivolveindex.iv_twitter_lists
       end
       if facebook?
-        @facebook_name = @facebook_auth_user.name
-        # how common are bios?  Let's IF the bio in.
-        @facebook_bio = @facebook_auth_user.bio
-        @facebook_birthday = @facebook_auth_user.birthday
         @facebook_int_friends = @facebook_auth_user.int_friends
         @facebook_avg_friends = @ivolveindex.iv_facebook_friends
         @facebook_int_likes = @facebook_auth_user.int_likes
@@ -38,6 +45,21 @@ class StaticPagesController < ApplicationController
         @facebook_avg_subscribers = @ivolveindex.iv_facebook_subscribers
         @facebook_int_subscribed_to = @facebook_auth_user.int_subscribed_to
         @facebook_avg_subscribed_to = @ivolveindex.iv_facebook_subscribed_to_you
+      else
+        @facebook_int_friends = 0
+        @facebook_avg_friends = @ivolveindex.iv_facebook_friends
+        @facebook_int_likes = 0
+        @facebook_avg_likes = @ivolveindex.iv_facebook_likes_sent
+        @facebook_int_posts = 0
+        @facebook_avg_posts = @ivolveindex.iv_facebook_posts
+        @facebook_int_statuses = 0
+        @facebook_avg_statuses = @ivolveindex.iv_facebook_statuses
+        @facebook_int_achievements = 0
+        @facebook_avg_achievements = @ivolveindex.iv_facebook_achievements
+        @facebook_int_subscribers = 0
+        @facebook_avg_subscribers = @ivolveindex.iv_facebook_subscribers
+        @facebook_int_subscribed_to = 0
+        @facebook_avg_subscribed_to = @ivolveindex.iv_facebook_subscribed_to_you
       end
       if instagram?
         @instagram_int_followers = @instagram_auth_user.int_followers
@@ -47,6 +69,15 @@ class StaticPagesController < ApplicationController
         @instagram_int_media = @instagram_auth_user.int_media
         @instagram_avg_media = @ivolveindex.iv_instagram_shared
         @instagram_int_likes_out = @instagram_auth_user.int_likes_out
+        @instagram_avg_likes_out = @ivolveindex.iv_instagram_likes_sent
+      else
+        @instagram_int_followers = 0
+        @instagram_avg_followers = @ivolveindex.iv_instagram_followers
+        @instagram_int_following = 0
+        @instagram_avg_following = @ivolveindex.iv_instagram_following
+        @instagram_int_media = 0
+        @instagram_avg_media = @ivolveindex.iv_instagram_shared
+        @instagram_int_likes_out = 0
         @instagram_avg_likes_out = @ivolveindex.iv_instagram_likes_sent
       end
       if foursquare?
@@ -63,6 +94,21 @@ class StaticPagesController < ApplicationController
         @foursquare_tips_count = @foursquare_auth_user.tips_count
         @foursquare_avg_tips = @ivolveindex.iv_foursquare_tips
         @foursquare_photos_count = @foursquare_auth_user.photos_count
+        @foursquare_avg_photos = @ivolveindex.iv_foursquare_photos
+      else
+        @foursquare_friends_count = 0
+        @foursquare_avg_friends = @ivolveindex.iv_foursquare_friends
+        @foursquare_following_count = 0
+        @foursquare_avg_following = @ivolveindex.iv_foursquare_following
+        @foursquare_checkins_count = 0
+        @foursquare_avg_checkins = @ivolveindex.iv_foursquare_checkins
+        @foursquare_badges_count = 0
+        @foursquare_avg_badges = @ivolveindex.iv_foursquare_badges
+        @foursquare_mayor_count = 0
+        @foursquare_avg_mayors = @ivolveindex.iv_foursquare_mayor
+        @foursquare_tips_count = 0
+        @foursquare_avg_tips = @ivolveindex.iv_foursquare_tips
+        @foursquare_photos_count = 0
         @foursquare_avg_photos = @ivolveindex.iv_foursquare_photos
       end
       if fitbit?
@@ -94,6 +140,35 @@ class StaticPagesController < ApplicationController
         @fitbit_best_tot_steps_int = @fitbit_auth_user.best_tot_steps_int
         @fitbit_avg_best_tot_steps = @ivolveindex.iv_fitbit_best_tot_steps
         @fitbit_best_tot_steps_date = @fitbit_auth_user.best_tot_steps_date
+      else
+        @fitbit_height_int = 0
+        @fitbit_avg_height = @ivolveindex.iv_fitbit_height
+        @fitbit_weight_int = 0
+        @fitbit_avg_weight = @ivolveindex.iv_fitbit_weight
+        @fitbit_stride_length_run_int = 0
+        @fitbit_avg_length_run = @ivolveindex.iv_fitbit_stride_length_run
+        @fitbit_stride_length_walk_int = 0
+        @fitbit_avg_length_walk = @ivolveindex.iv_fitbit_stride_length_walk
+        @fitbit_lifetime_tot_active_score_int = 0
+        @fitbit_avg_life_active_score = @ivolveindex.iv_fitbit_life_tot_active_score
+        @fitbit_best_tot_active_score_int = 0
+        @fitbit_avg_best_active_score = @ivolveindex.iv_fitbit_best_tot_active_score
+        @fitbit_best_tot_active_date = 0
+        @fitbit_lifetime_tot_cal_out_int = 0
+        @fitbit_avg_life_cal_out = @ivolveindex.iv_fitbit_life_tot_cal_out
+        @fitbit_best_tot_cal_out_int = 0
+        @fitbit_avg_best_cal_out = @ivolveindex.iv_fitbit_best_tot_cal_out
+        @fitbit_best_tot_cal_out_date = 0
+        @fitbit_lifetime_tot_dist_int = 0
+        @fitbit_avg_life_tot_dist = @ivolveindex.iv_fitbit_life_tot_dist
+        @fitbit_best_tot_dist_int = 0
+        @fitbit_avg_best_dist = @ivolveindex.iv_fitbit_best_tot_dist
+        @fitbit_best_tot_dist_date = 0
+        @fitbit_lifetime_tot_steps_int = 0
+        @fitbit_avg_life_tot_steps = @ivolveindex.iv_fitbit_life_tot_steps
+        @fitbit_best_tot_steps_int = 0
+        @fitbit_avg_best_tot_steps = @ivolveindex.iv_fitbit_best_tot_steps
+        @fitbit_best_tot_steps_date = 0
       end
   	end
   end
