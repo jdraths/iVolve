a07997dda50124aeb25a2d7ba2d6cd559369fcff
jdraths <jdraths@alumni.nd.edu>
2013-12-21 14:58:27 -0800

    change line graphs to display one day less of data than was requested, ie., request 8 days of data (7.days.ago), so that the 7 days of data that are displayed (6.days.ago) calculate today-yesterday properly

--- app/helpers/graph_helper.rb
+++ app/helpers/graph_helper.rb
@@ -247,28 +247,28 @@ module GraphHelper
 		if !@linkedin_graph.first.nil?
 			@linkedin_graph_user = LinkedinUser.where("uid = ?", @linkedin_graph.first['uid'])
 			if !@linkedin_graph_user.nil?
-				linkedin_data_by_day = @linkedin_graph_user.connections_line_by_date(6.days.ago)
+				linkedin_data_by_day = @linkedin_graph_user.connections_line_by_date(7.days.ago)
 			end
 		end
 		@twitter_graph = Authorization.where("user_id = ?", user).where("provider = ?", "twitter")
 		if !@twitter_graph.first.nil?
 			@twitter_graph_user = TwitterUser.where("uid = ?", @twitter_graph.first['uid'])
 			if !@twitter_graph_user.nil?
-				twitter_data_by_day = @twitter_graph_user.connections_line_by_date(6.days.ago)
+				twitter_data_by_day = @twitter_graph_user.connections_line_by_date(7.days.ago)
 			end
 		end
 		@facebook_graph = Authorization.where("user_id = ?", user).where("provider = ?", "facebook")
 		if !@facebook_graph.first.nil?
 			@facebook_graph_user = FacebookUser.where("uid = ?", @facebook_graph.first['uid'])
 			if !@facebook_graph_user.nil?
-				facebook_data_by_day = @facebook_graph_user.connections_line_by_date(6.days.ago)
+				facebook_data_by_day = @facebook_graph_user.connections_line_by_date(7.days.ago)
 			end
 		end
 		@instagram_graph = Authorization.where("user_id = ?", user).where("provider = ?", "instagram")
 		if !@instagram_graph.first.nil?
 			@instagram_graph_user = InstagramUser.where("uid = ?", @instagram_graph.first['uid'])
 			if !@instagram_graph_user.nil?
-				instagram_data_by_day = @instagram_graph_user.connections_line_by_date(6.days.ago)
+				instagram_data_by_day = @instagram_graph_user.connections_line_by_date(7.days.ago)
 			end
 		end
 
@@ -276,7 +276,7 @@ module GraphHelper
 		if !@foursquare_graph.first.nil?
 			@foursquare_graph_user = FoursquareUser.where("uid = ?", @foursquare_graph.first['uid'])
 			if !@foursquare_graph_user.nil?
-				foursquare_data_by_day = @foursquare_graph_user.connections_line_by_date(6.days.ago)
+				foursquare_data_by_day = @foursquare_graph_user.connections_line_by_date(7.days.ago)
 			end
 		end
 
