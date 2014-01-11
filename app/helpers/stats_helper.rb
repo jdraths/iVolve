a04c814f58acd69aadc99def97bfaddb03089ed2
jdraths <jdraths@alumni.nd.edu>
2014-01-11 13:24:18 -0800

    update stats_helper to ignore @ivolve_index when it is 0... which will be whenever it is on a local dev machine that has not been used before.

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -7,29 +7,32 @@ module StatsHelper
     # => see users_controller.rb show action for usage.
     def stats_view
         if signed_in?
-                @ivolveindex = IvolveIndex.first
-            if linkedin?
-                @linkedin_int_connections = @linkedin_auth_user.connections_size
-                @linkedin_int_group_memberships = @linkedin_auth_user.group_memberships_size
-                @linkedin_int_job_suggestions = @linkedin_auth_user.job_suggestions_size
-                @linkedin_int_job_bookmarks = @linkedin_auth_user.job_bookmarks_size
-                @linkedin_int_shares = @linkedin_auth_user.shares_size
+            @ivolveindex = IvolveIndex.first
+            if !@ivolveindex.nil?
                 @linkedin_avg_connections = @ivolveindex.iv_linkedin_connections_int
                 @linkedin_avg_group_memberships = @ivolveindex.iv_linkedin_group_memberships_int
                 @linkedin_avg_job_suggestions = @ivolveindex.iv_linkedin_job_suggestions_int
                 @linkedin_avg_job_bookmarks = @ivolveindex.iv_linkedin_job_bookmarks_int
                 @linkedin_avg_shares = @ivolveindex.iv_linkedin_shares_int
             else
+                @linkedin_avg_connections = 0
+                @linkedin_avg_group_memberships = 0
+                @linkedin_avg_job_suggestions = 0
+                @linkedin_avg_job_bookmarks = 0
+                @linkedin_avg_shares = 0
+            end
+            if linkedin?
+                @linkedin_int_connections = @linkedin_auth_user.connections_size
+                @linkedin_int_group_memberships = @linkedin_auth_user.group_memberships_size
+                @linkedin_int_job_suggestions = @linkedin_auth_user.job_suggestions_size
+                @linkedin_int_job_bookmarks = @linkedin_auth_user.job_bookmarks_size
+                @linkedin_int_shares = @linkedin_auth_user.shares_size
+            else
                 @linkedin_int_connections = 0
-                @linkedin_avg_connections = @ivolveindex.iv_linkedin_connections_int
                 @linkedin_int_group_memberships = 0
-                @linkedin_avg_group_memberships = @ivolveindex.iv_linkedin_group_memberships_int
                 @linkedin_int_job_suggestions = 0
-                @linkedin_avg_job_suggestions = @ivolveindex.iv_linkedin_job_suggestions_int
                 @linkedin_int_job_bookmarks = 0
-                @linkedin_avg_job_bookmarks = @ivolveindex.iv_linkedin_job_bookmarks_int
                 @linkedin_int_shares = 0
-                @linkedin_avg_shares = @ivolveindex.iv_linkedin_shares_int
             end
             if @linkedin_int_connections.nil?
                 @linkedin_int_connections = 0
@@ -61,16 +64,7 @@ module StatsHelper
             if @linkedin_avg_shares.nil?
                 @linkedin_avg_shares = 0
             end
-
-            if twitter?   # is current_user an authorized twitter user with data saved?
-                @twitter_int_friends = @twitter_auth_user.friends_int_count
-                @twitter_int_followers = @twitter_auth_user.followers_int_count
-                @twitter_int_sent = @twitter_auth_user.tweet_int_count
-                @twitter_int_favorites = @twitter_auth_user.favorite_int_count
-                @twitter_int_lists = @twitter_auth_user.listed_int_count
-                @twitter_num_retweets_of_me = @twitter_auth_user.num_retweets_of_me
-                @twitter_num_mentions_of_me = @twitter_auth_user.num_mentions_of_me
-                @twitter_num_retweets_by_me = @twitter_auth_user.num_retweets_by_me
+            if !@ivolveindex.nil?
                 @twitter_avg_friends = @ivolveindex.iv_twitter_friends
                 @twitter_avg_followers = @ivolveindex.iv_twitter_follwers
                 @twitter_avg_sent = @ivolveindex.iv_twitter_tweets_sent
@@ -80,22 +74,33 @@ module StatsHelper
                 @twitter_avg_mentions_of_me = @ivolveindex.iv_twitter_mentions_of_me
                 @twitter_avg_retweets_by_me = @ivolveindex.iv_twitter_retweets_by_me
             else
+                @twitter_avg_friends = 0
+                @twitter_avg_followers = 0
+                @twitter_avg_sent = 0
+                @twitter_avg_fav = 0
+                @twitter_avg_lists = 0
+                @twitter_avg_retweets_of_me = 0
+                @twitter_avg_mentions_of_me = 0
+                @twitter_avg_retweets_by_me = 0
+            end
+            if twitter?   # is current_user an authorized twitter user with data saved?
+                @twitter_int_friends = @twitter_auth_user.friends_int_count
+                @twitter_int_followers = @twitter_auth_user.followers_int_count
+                @twitter_int_sent = @twitter_auth_user.tweet_int_count
+                @twitter_int_favorites = @twitter_auth_user.favorite_int_count
+                @twitter_int_lists = @twitter_auth_user.listed_int_count
+                @twitter_num_retweets_of_me = @twitter_auth_user.num_retweets_of_me
+                @twitter_num_mentions_of_me = @twitter_auth_user.num_mentions_of_me
+                @twitter_num_retweets_by_me = @twitter_auth_user.num_retweets_by_me
+            else
                 @twitter_int_friends = 0
-                @twitter_avg_friends = @ivolveindex.iv_twitter_friends
                 @twitter_int_followers = 0
-                @twitter_avg_followers = @ivolveindex.iv_twitter_follwers
                 @twitter_int_sent = 0
-                @twitter_avg_sent = @ivolveindex.iv_twitter_tweets_sent
                 @twitter_int_favorites = 0
-                @twitter_avg_fav = @ivolveindex.iv_twitter_tweets_favd
                 @twitter_int_lists = 0
-                @twitter_avg_lists = @ivolveindex.iv_twitter_lists
                 @twitter_num_retweets_of_me = 0
-                @twitter_avg_retweets_of_me = @ivolveindex.iv_twitter_retweets_of_me
                 @twitter_num_mentions_of_me = 0
-                @twitter_avg_mentions_of_me = @ivolveindex.iv_twitter_mentions_of_me
                 @twitter_num_retweets_by_me = 0
-                @twitter_avg_retweets_by_me = @ivolveindex.iv_twitter_retweets_by_me
             end
             if @twitter_int_friends.nil?
                 @twitter_int_friends = 0
@@ -146,14 +151,7 @@ module StatsHelper
                 @twitter_avg_retweets_by_me = 0
             end
 
-            if facebook? # is current_user an authorized facebook user with data saved?
-                @facebook_int_friends = @facebook_auth_user.int_friends
-                @facebook_int_likes = @facebook_auth_user.int_likes
-                @facebook_int_posts = @facebook_auth_user.int_posts
-                @facebook_int_statuses = @facebook_auth_user.int_statuses
-                @facebook_int_achievements = @facebook_auth_user.int_achievements
-                @facebook_int_subscribers = @facebook_auth_user.int_subscribers
-                @facebook_int_subscribed_to = @facebook_auth_user.int_subscribed_to
+            if !@ivolveindex.nil?
                 @facebook_avg_friends = @ivolveindex.iv_facebook_friends
                 @facebook_avg_likes = @ivolveindex.iv_facebook_likes_sent
                 @facebook_avg_posts = @ivolveindex.iv_facebook_posts
@@ -162,20 +160,30 @@ module StatsHelper
                 @facebook_avg_subscribers = @ivolveindex.iv_facebook_subscribers
                 @facebook_avg_subscribed_to = @ivolveindex.iv_facebook_subscribed_to_you
             else
+                @facebook_avg_friends = 0
+                @facebook_avg_likes = 0
+                @facebook_avg_posts = 0
+                @facebook_avg_statuses = 0
+                @facebook_avg_achievements = 0
+                @facebook_avg_subscribers = 0
+                @facebook_avg_subscribed_to = 0
+            end
+            if facebook? # is current_user an authorized facebook user with data saved?
+                @facebook_int_friends = @facebook_auth_user.int_friends
+                @facebook_int_likes = @facebook_auth_user.int_likes
+                @facebook_int_posts = @facebook_auth_user.int_posts
+                @facebook_int_statuses = @facebook_auth_user.int_statuses
+                @facebook_int_achievements = @facebook_auth_user.int_achievements
+                @facebook_int_subscribers = @facebook_auth_user.int_subscribers
+                @facebook_int_subscribed_to = @facebook_auth_user.int_subscribed_to
+            else
                 @facebook_int_friends = 0
-                @facebook_avg_friends = @ivolveindex.iv_facebook_friends
                 @facebook_int_likes = 0
-                @facebook_avg_likes = @ivolveindex.iv_facebook_likes_sent
                 @facebook_int_posts = 0
-                @facebook_avg_posts = @ivolveindex.iv_facebook_posts
                 @facebook_int_statuses = 0
-                @facebook_avg_statuses = @ivolveindex.iv_facebook_statuses
                 @facebook_int_achievements = 0
-                @facebook_avg_achievements = @ivolveindex.iv_facebook_achievements
                 @facebook_int_subscribers = 0
-                @facebook_avg_subscribers = @ivolveindex.iv_facebook_subscribers
                 @facebook_int_subscribed_to = 0
-                @facebook_avg_subscribed_to = @ivolveindex.iv_facebook_subscribed_to_you
             end
             if @facebook_int_friends.nil?
                 @facebook_int_friends = 0
@@ -220,24 +228,27 @@ module StatsHelper
                 @facebook_avg_subscribed_to = 0
             end
 
-            if instagram?
-                @instagram_int_followers = @instagram_auth_user.int_followers
-                @instagram_int_following = @instagram_auth_user.int_following
-                @instagram_int_media = @instagram_auth_user.int_media
-                @instagram_int_likes_out = @instagram_auth_user.int_likes_out
+            if !@ivolveindex.nil?
                 @instagram_avg_followers = @ivolveindex.iv_instagram_followers
                 @instagram_avg_following = @ivolveindex.iv_instagram_following
                 @instagram_avg_media = @ivolveindex.iv_instagram_shared
                 @instagram_avg_likes_out = @ivolveindex.iv_instagram_likes_sent
             else
+                @instagram_avg_followers = 0
+                @instagram_avg_following = 0
+                @instagram_avg_media = 0
+                @instagram_avg_likes_out = 0
+            end
+            if instagram?
+                @instagram_int_followers = @instagram_auth_user.int_followers
+                @instagram_int_following = @instagram_auth_user.int_following
+                @instagram_int_media = @instagram_auth_user.int_media
+                @instagram_int_likes_out = @instagram_auth_user.int_likes_out
+            else
                 @instagram_int_followers = 0
-                @instagram_avg_followers = @ivolveindex.iv_instagram_followers
                 @instagram_int_following = 0
-                @instagram_avg_following = @ivolveindex.iv_instagram_following
                 @instagram_int_media = 0
-                @instagram_avg_media = @ivolveindex.iv_instagram_shared
                 @instagram_int_likes_out = 0
-                @instagram_avg_likes_out = @ivolveindex.iv_instagram_likes_sent
             end
             if @instagram_int_followers.nil?
                 @instagram_int_followers = 0
@@ -263,15 +274,7 @@ module StatsHelper
             if @instagram_avg_likes_out.nil?
                 @instagram_avg_likes_out = 0
             end
-
-            if foursquare?
-                @foursquare_friends_count = @foursquare_auth_user.friends_count
-                @foursquare_following_count = @foursquare_auth_user.following_count
-                @foursquare_checkins_count = @foursquare_auth_user.checkins_count
-                @foursquare_badges_count = @foursquare_auth_user.badges_count
-                @foursquare_mayor_count = @foursquare_auth_user.mayor_count
-                @foursquare_tips_count = @foursquare_auth_user.tips_count
-                @foursquare_photos_count = @foursquare_auth_user.photos_count
+            if !@ivolveindex.nil?
                 @foursquare_avg_friends = @ivolveindex.iv_foursquare_friends
                 @foursquare_avg_following = @ivolveindex.iv_foursquare_following
                 @foursquare_avg_checkins = @ivolveindex.iv_foursquare_checkins
@@ -280,20 +283,30 @@ module StatsHelper
                 @foursquare_avg_tips = @ivolveindex.iv_foursquare_tips
                 @foursquare_avg_photos = @ivolveindex.iv_foursquare_photos
             else
+                @foursquare_avg_friends = 0
+                @foursquare_avg_following = 0
+                @foursquare_avg_checkins = 0
+                @foursquare_avg_badges = 0
+                @foursquare_avg_mayors = 0
+                @foursquare_avg_tips = 0
+                @foursquare_avg_photos = 0
+            end
+            if foursquare?
+                @foursquare_friends_count = @foursquare_auth_user.friends_count
+                @foursquare_following_count = @foursquare_auth_user.following_count
+                @foursquare_checkins_count = @foursquare_auth_user.checkins_count
+                @foursquare_badges_count = @foursquare_auth_user.badges_count
+                @foursquare_mayor_count = @foursquare_auth_user.mayor_count
+                @foursquare_tips_count = @foursquare_auth_user.tips_count
+                @foursquare_photos_count = @foursquare_auth_user.photos_count
+            else
                 @foursquare_friends_count = 0
-                @foursquare_avg_friends = @ivolveindex.iv_foursquare_friends
                 @foursquare_following_count = 0
-                @foursquare_avg_following = @ivolveindex.iv_foursquare_following
                 @foursquare_checkins_count = 0
-                @foursquare_avg_checkins = @ivolveindex.iv_foursquare_checkins
                 @foursquare_badges_count = 0
-                @foursquare_avg_badges = @ivolveindex.iv_foursquare_badges
                 @foursquare_mayor_count = 0
-                @foursquare_avg_mayors = @ivolveindex.iv_foursquare_mayor
                 @foursquare_tips_count = 0
-                @foursquare_avg_tips = @ivolveindex.iv_foursquare_tips
                 @foursquare_photos_count = 0
-                @foursquare_avg_photos = @ivolveindex.iv_foursquare_photos
             end
             if @foursquare_friends_count.nil?
                 @foursquare_friends_count = 0
@@ -337,20 +350,7 @@ module StatsHelper
             if @foursquare_avg_photos.nil?
                 @foursquare_avg_photos = 0
             end
-
-            if fitbit?
-                @fitbit_height_int = @fitbit_auth_user.height_int
-                @fitbit_weight_int = @fitbit_auth_user.weight_int
-                @fitbit_stride_length_run_int = @fitbit_auth_user.stride_length_run_int
-                @fitbit_stride_length_walk_int = @fitbit_auth_user.stride_length_walk_int
-                @fitbit_lifetime_tot_active_score_int = @fitbit_auth_user.lifetime_tot_active_score_int
-                @fitbit_best_tot_active_score_int = @fitbit_auth_user.best_tot_active_score_int
-                @fitbit_lifetime_tot_cal_out_int = @fitbit_auth_user.lifetime_tot_cal_out_int
-                @fitbit_best_tot_cal_out_int = @fitbit_auth_user.best_tot_cal_out_int
-                @fitbit_lifetime_tot_dist_int = @fitbit_auth_user.lifetime_tot_dist_int
-                @fitbit_best_tot_dist_int = @fitbit_auth_user.best_tot_dist_int
-                @fitbit_lifetime_tot_steps_int = @fitbit_auth_user.lifetime_tot_steps_int
-                @fitbit_best_tot_steps_int = @fitbit_auth_user.best_tot_steps_int
+            if !@ivolveindex.nil?
                 @fitbit_avg_height = @ivolveindex.iv_fitbit_height
                 @fitbit_avg_weight = @ivolveindex.iv_fitbit_weight
                 @fitbit_avg_length_run = @ivolveindex.iv_fitbit_stride_length_run
@@ -364,30 +364,37 @@ module StatsHelper
                 @fitbit_avg_life_tot_steps = @ivolveindex.iv_fitbit_life_tot_steps
                 @fitbit_avg_best_tot_steps = @ivolveindex.iv_fitbit_best_tot_steps
             else
+                @fitbit_avg_height = 0
+                @fitbit_avg_weight = 0
+                @fitbit_avg_length_run = 0
+                @fitbit_avg_length_walk = 0
+            end
+            if fitbit?
+                @fitbit_height_int = @fitbit_auth_user.height_int
+                @fitbit_weight_int = @fitbit_auth_user.weight_int
+                @fitbit_stride_length_run_int = @fitbit_auth_user.stride_length_run_int
+                @fitbit_stride_length_walk_int = @fitbit_auth_user.stride_length_walk_int
+                @fitbit_lifetime_tot_active_score_int = @fitbit_auth_user.lifetime_tot_active_score_int
+                @fitbit_best_tot_active_score_int = @fitbit_auth_user.best_tot_active_score_int
+                @fitbit_lifetime_tot_cal_out_int = @fitbit_auth_user.lifetime_tot_cal_out_int
+                @fitbit_best_tot_cal_out_int = @fitbit_auth_user.best_tot_cal_out_int
+                @fitbit_lifetime_tot_dist_int = @fitbit_auth_user.lifetime_tot_dist_int
+                @fitbit_best_tot_dist_int = @fitbit_auth_user.best_tot_dist_int
+                @fitbit_lifetime_tot_steps_int = @fitbit_auth_user.lifetime_tot_steps_int
+                @fitbit_best_tot_steps_int = @fitbit_auth_user.best_tot_steps_int
+            else
                 @fitbit_height_int = 0
-                @fitbit_avg_height = @ivolveindex.iv_fitbit_height
                 @fitbit_weight_int = 0
-                @fitbit_avg_weight = @ivolveindex.iv_fitbit_weight
                 @fitbit_stride_length_run_int = 0
-                @fitbit_avg_length_run = @ivolveindex.iv_fitbit_stride_length_run
                 @fitbit_stride_length_walk_int = 0
-                @fitbit_avg_length_walk = @ivolveindex.iv_fitbit_stride_length_walk
                 @fitbit_lifetime_tot_active_score_int = 0
-                @fitbit_avg_life_active_score = @ivolveindex.iv_fitbit_life_tot_active_score
                 @fitbit_best_tot_active_score_int = 0
-                @fitbit_avg_best_active_score = @ivolveindex.iv_fitbit_best_tot_active_score
                 @fitbit_lifetime_tot_cal_out_int = 0
-                @fitbit_avg_life_cal_out = @ivolveindex.iv_fitbit_life_tot_cal_out
                 @fitbit_best_tot_cal_out_int = 0
-                @fitbit_avg_best_cal_out = @ivolveindex.iv_fitbit_best_tot_cal_out
                 @fitbit_lifetime_tot_dist_int = 0
-                @fitbit_avg_life_tot_dist = @ivolveindex.iv_fitbit_life_tot_dist
                 @fitbit_best_tot_dist_int = 0
-                @fitbit_avg_best_dist = @ivolveindex.iv_fitbit_best_tot_dist
                 @fitbit_lifetime_tot_steps_int = 0
-                @fitbit_avg_life_tot_steps = @ivolveindex.iv_fitbit_life_tot_steps
                 @fitbit_best_tot_steps_int = 0
-                @fitbit_avg_best_tot_steps = @ivolveindex.iv_fitbit_best_tot_steps
             end
             if @fitbit_height_int.nil?
                 @fitbit_height_int = 0
